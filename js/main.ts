87366957ba1f4e48040a9e49daf4e6bdccbf93dc
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-02-10 15:04:39 -0500

    temp vite dev

--- js/main.ts
+++ js/main.ts
@@ -6,13 +6,15 @@
  * This file will be bundled and loaded in the SWC application. Excess imports should be avoided to keep this
  * bundle as small as possible.
  */
+// https://vite.dev/guide/backend-integration
+import 'vite/modulepreload-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMClient from 'react-dom/client'
-import * as SRC from 'synapse-react-client'
+// import * as SRC from 'synapse-react-client'
 import * as ReactQuery from '@tanstack/react-query'
-import Grid2 from '@mui/material/Grid2'
-import * as Moment from 'moment'
+// import Grid2 from '@mui/material/Grid2'
+// import * as Moment from 'moment'
 
 self.React = React
 self.ReactDOM = ReactDOM
@@ -22,13 +24,13 @@ self.ReactDOMClient = ReactDOMClient
 self.ReactQuery = ReactQuery
 
 // @ts-expect-error
-self.Moment = Moment
+// self.Moment = Moment
 
-const MaterialUI = {
-  Grid2,
-}
+// const MaterialUI = {
+//   Grid2,
+// }
 
 // @ts-expect-error
-self.MaterialUI = MaterialUI
+// self.MaterialUI = MaterialUI
 // @ts-expect-error
-self.SRC = SRC
+// self.SRC = SRC
--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,30 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
  * Vite config to generate the ESM & CJS bundles for Synapse React Client.
  */
 const config = defineConfig({
-  server: { port: 3000 },
+  server: {
+    // port: 5173,
+    cors: {
+      // the origin you will be accessing via browser
+      origin: ['http://localhost:8888', 'http://127.0.0.1:8888'],
+    },
+  },
+  // optimizeDeps: {
+  //   exclude: ['react', 'react-dom'],
+  // },
   build: {
-    lib: {
-      entry: 'js/swc-modules.ts',
-      fileName: 'swc-modules',
-      formats: ['es'],
-    },
-    outDir: './src/main/webapp/generated',
-    emptyOutDir: false,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
+    manifest: true,
+    // lib: {
+    //   entry: 'js/swc-modules.ts',
+    //   fileName: 'swc-modules',
+    //   formats: ['es'],
+    // },
+    // outDir: './src/main/webapp/generated',
+    // emptyOutDir: false,
+    // commonjsOptions: {
+    //   transformMixedEsModules: true,
+    // },
     rollupOptions: {
+      input: 'js/main.ts',
       onwarn(warning, warn) {
         // Suppress "Module level directives cause errors when bundled" warnings
         if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
